0b18d3f87f9e4eaad159ac532151c66eac114909
xelendt <m@xelendt.com>
2017-11-11 13:52:28 -0500

    Done page 1/14

---
+++ store_apparel_tags.js
@@ -0,0 +1,102 @@
+const apparel_items = [{
+// Amaal Leggings
+// https://cdn.shopify.com/s/files/1/2463/4523/products/1_ba8e1802-8a49-450c-9333-a2f2b7e4791a_360x.jpg?v=1508425248
+"Amaal Leggings": {
+	"Apparel" : ["Sarong", "Capris"],
+	"Color" : ["DarkSalmon", "DimGray", "LightSlateGray", "PaleVioletRed", "DarkGray"],
+	"Apparel_h" : ["Leggings", "Tights"],
+	"Color_h" : ["Purple"]
+},
+
+// Ankle Strap Leggings
+// https://cdn.shopify.com/s/files/1/2463/4523/products/d1cea9ff4d0f26dc9c52caab96124fe0_540x.jpg?v=1508425179
+"Ankle Strap Leggings" : {
+	"Apparel" : ["Tights", "Capris", "Leggings"],
+	"Color" : ["Black"],
+	"Apparel_h" : [],
+	"Color_h" : []
+},
+
+// Anna High Performance Tank - Black
+// https://cdn.shopify.com/s/files/1/2463/4523/products/1_c4da334d-873f-4161-8b19-7285a6c7e2e4_360x.jpg?v=1508425284
+"Anna High Performance Tank - Black" : {
+	"Apparel" : ["Tank Top", "Sports Bra", "Bodysuit"],
+	"Color" : ["Black"],
+	"Apparel_h": [],
+	"Color_h": []
+},
+
+// Anna High Performance Tank - Lemon
+// https://cdn.shopify.com/s/files/1/2463/4523/products/1_934ed483-d229-45c9-988d-cb4b84fa0d66_360x.jpg?v=1508425302
+"Anna High Performance Tank - Lemon" : {
+	"Apparel": ["Tank Top", "Sports Bra", "Bodysuit"],
+	"Color": ["PaleGoldenRod"],
+	"Apparel_h": [],
+	"Color_h": ["Lemon"]
+},
+
+// 3/4 Guggenheim Leggnings
+// https://cdn.shopify.com/s/files/1/2463/4523/products/a3c2861cb303ef8ee9f0769f44292958_540x.jpg?v=1508425143
+"3/4 Guggenheim Leggings": {
+	"Apparel": ["Capris", "Tights", "Overalls"],
+	"Color": ["LightSteelBlue", "White", "DarkSlateBlue"],
+	"Apparel_h": ["Leggings"],
+	"Color_h": []
+},
+
+// Ambu Leggings
+// https://cdn.shopify.com/s/files/1/2463/4523/products/1_b5f36b8a-53ba-44ea-bf6e-0a6b6e86fa83_540x.jpg?v=1508425309
+"Ambu Leggings": {
+	"Apparel": ["Capris", "Bodysuit", "Tights"],
+	"Color": ["DarkSlateGray", "RosyBrown", "Tan"],
+	"Apparel_h": ["Leggings"],
+	"Color_h": ["Pink"]
+},
+
+// Anise Leggings
+// https://cdn.shopify.com/s/files/1/2463/4523/products/1_efc195b5-c2d4-4652-b58a-586ed3334882_540x.jpg?v=1508425311
+"Anise Leggings": {
+	"Apparel": ["Women's Shorts", "Capris", "Sarong"],
+	"Color": ["Black"],
+	"Apparel_h": ["Leggings", "Tights"],
+	"Color_h": ["DarkSlateBlue"]
+},
+
+// Anna High Performance Leggings
+// https://cdn.shopify.com/s/files/1/2463/4523/products/1_65d43591-3f71-4225-a8ac-35083a8874bb_360x.jpg?v=1508425305
+"Anna High Performance Leggings": {
+	"Apparel": ["Capris", "Tights", "Jumpsuit"],
+	"Color": ["PaleGoldenRod", "DimGray"],
+	"Apparel_h": ["Leggings"],
+	"Color_h": ["Black"]
+},
+
+// END OF PAGE 1
+
+// END OF PAGE 2
+
+// END OF PAGE 3
+
+// END OF PAGE 4
+
+// END OF PAGE 5
+
+// END OF PAGE 6
+
+// END OF PAGE 7
+
+// END OF PAGE 8
+
+// END OF PAGE 9
+
+// END OF PAGE 10
+
+// END OF PAGE 11
+
+// END OF PAGE 12
+
+// END OF PAGE 13
+
+// END OF PAGE 14
+
+}];
