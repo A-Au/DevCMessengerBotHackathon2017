d04ed8db60491ada3c6868a41fa153fbd9f222e5
Andrew Au <iamandrewau@gmail.com>
2017-11-14 04:08:08 -0500

    cleaned up some more code for linter

--- src/app.js
+++ src/app.js
@@ -25,24 +25,24 @@ const
   Greeting = require('greeting'),
   apparel = require('./apparel_list.js');
 
-var app = express();
+let appClarifai = new Clarifai.App({
+ apiKey: 'cc1cdb1c7b6145dc868d5e00b8965594'
+});
+
+let app = express();
 app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(express.static('public'));
 
-var app_clarifai = new Clarifai.App({
- apiKey: 'cc1cdb1c7b6145dc868d5e00b8965594'
-});
-
 function clarifai_predict(url){
-  var colour = app_clarifai.models.predict(Clarifai.COLOR_MODEL, url).then(
+  var colour = appClarifai.models.predict(Clarifai.COLOR_MODEL, url).then(
       (response, error) => {
         //console.log(JSON.stringify(response.outputs[0].data));
         return response.outputs[0].data;
       }
     );
-  var apparel_type = app_clarifai.models.predict(Clarifai.APPAREL_MODEL, url).then(
+  var apparel_type = appClarifai.models.predict(Clarifai.APPAREL_MODEL, url).then(
         (response, error) => {
           //console.log(JSON.stringify(response.outputs[0].data));
           return response.outputs[0].data;
@@ -51,8 +51,8 @@ function clarifai_predict(url){
 
   const prediction = { colour, apparel_type };
   console.log(JSON.stringify(prediction.colour.then(res => res)));
-  //prediction.then(r => r.colour.then(res => console.log("aaaa" + JSON.stringify(res))));
-  console.log("a");
+  //prediction.then(r => r.colour.then(res => console.log('aaaa' + JSON.stringify(res))));
+  console.log('a');
   return prediction;
 }
 
@@ -95,7 +95,7 @@ const HOST_URL = (process.env.HOST_URL) ?
 
 // make sure that everything has been properly configured
 if (!(FB_APP_SECRET && FB_VALIDATION_TOKEN && FB_PAGE_ACCESS_TOKEN && SHOPIFY_SHOP_NAME && SHOPIFY_API_KEY && SHOPIFY_API_PASSWORD && HOST_URL)) {
-  console.error("Missing config values");
+  console.error('Missing config values');
   process.exit(1);
 }
 
@@ -115,11 +115,11 @@ const shopify = new Shopify({
  *
  */
 function verifyRequestSignature(req, res, buf) {
-  var signature = req.headers["x-hub-signature"];
+  var signature = req.headers['x-hub-signature'];
 
   if (!signature) {
     // In DEV, log an error. In PROD, throw an error.
-    console.error("Couldn't validate the signature.");
+    console.error('Couldn\'t validate the signature.');
   } else {
     var elements = signature.split('=');
     var method = elements[0];
@@ -129,11 +129,11 @@ function verifyRequestSignature(req, res, buf) {
                         .update(buf)
                         .digest('hex');
 
-    //console.log("signatureHash: " + signatureHash);
-    //console.log("expectedHash: " + expectedHash);
+    //console.log('signatureHash: ' + signatureHash);
+    //console.log('expectedHash: ' + expectedHash);
 
     if (signatureHash != expectedHash) {
-      throw new Error("Couldn't validate the request signature.");
+      throw new Error('Couldn\'t validate the request signature.');
     }
   }
 }
@@ -146,10 +146,10 @@ function verifyRequestSignature(req, res, buf) {
 app.get('/webhook', function(req, res) {
   if (req.query['hub.mode'] === 'subscribe' &&
       req.query['hub.verify_token'] === FB_VALIDATION_TOKEN) {
-    console.log("[app.get] Validating webhook");
+    console.log('[app.get] Validating webhook');
     res.status(200).send(req.query['hub.challenge']);
   } else {
-    console.error("Failed validation. Make sure the validation tokens match.");
+    console.error('Failed validation. Make sure the validation tokens match.');
     res.sendStatus(403);          
   }  
 });
@@ -160,19 +160,19 @@ app.get('/webhook', function(req, res) {
 app.get('/product_description', function(req, res) {
   var product_id = req.query['id'];
   if (product_id !== 'null') {
-    console.log("[app.get] product id:" + product_id);
+    console.log('[app.get] product id:' + product_id);
     var sh_product = shopify.product.get(product_id);
     sh_product.then(function(product) {
       console.log(product.options[0].values);
       res.status(200).send(product.body_html);
     }, function(error) {
-      console.error("Error retrieving product");
-      res.sendStatus(400).send("Error retrieving product");
+      console.error('Error retrieving product');
+      res.sendStatus(400).send('Error retrieving product');
     });
     
   } else {
-    console.error("Product id is required");
-    res.sendStatus(400).send("Product id is required");          
+    console.error('Product id is required');
+    res.sendStatus(400).send('Product id is required');          
   }  
 });
 
@@ -203,7 +203,7 @@ app.post('/webhook', function (req, res) {
 
         let propertyNames = [];
         for (var prop in messagingEvent) { propertyNames.push(prop)}
-        console.log("[app.post] Webhook received a messagingEvent with properties: ", propertyNames.join());
+        console.log('[app.post] Webhook received a messagingEvent with properties: ', propertyNames.join());
         
         if (messagingEvent.message) {
           // someone sent a message
@@ -218,7 +218,7 @@ app.post('/webhook', function (req, res) {
           receivedPostback(messagingEvent);
 
         } else {
-          console.log("[app.post] Webhook is not prepared to handle this message.");
+          console.log('[app.post] Webhook is not prepared to handle this message.');
 
         }
       });
@@ -235,9 +235,9 @@ function parseColor(hash){
 
   var ret = [-1,-1,-1];
 
-  ret[0] = parseInt("0x"+hash.substr(1,2));
-  ret[1] = parseInt("0x"+hash.substr(3,2));
-  ret[2] = parseInt("0x"+hash.substr(5,2));
+  ret[0] = parseInt('0x'+hash.substr(1,2));
+  ret[1] = parseInt('0x'+hash.substr(3,2));
+  ret[2] = parseInt('0x'+hash.substr(5,2));
   //console.log(hash);
   //console.log(hash.substr(1,2));
   //console.log(hash.substr(3,2));
@@ -329,7 +329,7 @@ function matchItem(item_type, usr_colors){
           numcomps ++;
         }
       }
-      console.log("Color distance for (%s) is (%d)", store.apparel_tops[i], curDist/numcomps);
+      console.log('Color distance for (%s) is (%d)', store.apparel_tops[i], curDist/numcomps);
       distances.push([i, curDist/numcomps]);
       curDist = 0;
       numcomps = 0;
@@ -354,7 +354,7 @@ function matchItem(item_type, usr_colors){
           numcomps ++;
         }
       }
-      console.log("Color distance for (%s) is (%d)", store.apparel_bottoms[i], curDist/numcomps);
+      console.log('Color distance for (%s) is (%d)', store.apparel_bottoms[i], curDist/numcomps);
       distances.push([i, curDist/numcomps]);
       curDist = 0;
       numcomps = 0;
@@ -390,17 +390,17 @@ function receivedMessage(event) {
 
   //shopify.product.get(229020762139).then(res => console.log(JSON.stringify(res)));
 
-  console.log("[receivedMessage] user (%d) page (%d) timestamp (%d) and message (%s)", 
+  console.log('[receivedMessage] user (%d) page (%d) timestamp (%d) and message (%s)', 
     senderID, pageID, timeOfMessage, JSON.stringify(message));
   console.log(message);
 
   if (message.attachments && message.attachments.length >= 1 && message.attachments[0] && message.attachments[0].payload && message.attachments[0].payload.url) {
     var url = message.attachments[0].payload.url;
-    console.log("[receivedMessage] image url: (%s)", url);
+    console.log('[receivedMessage] image url: (%s)', url);
     var prediction = clarifai_predict(url);
     console.log(JSON.stringify(prediction));
     if (message.quick_reply) {
-      console.log("[receivedMessage] quick_reply.payload (%s)", 
+      console.log('[receivedMessage] quick_reply.payload (%s)', 
         message.quick_reply.payload);
       handleQuickReplyResponse(event);
       return;
@@ -408,7 +408,7 @@ function receivedMessage(event) {
     if (prediction) {
       const col = prediction.colour.then(res => res.colors);//[0].w3c.name);
       const type = prediction.apparel_type.then(res => res.concepts);//[0].name);
-      prediction.colour.then(res => console.log("aaaa" + typeof res.colors[0].w3c.name));
+      prediction.colour.then(res => console.log('aaaa' + typeof res.colors[0].w3c.name));
       console.log(typeof type);
       console.log(type);
       col.then(co => {type.then(ty => {
@@ -433,7 +433,7 @@ function receivedMessage(event) {
             foundtype = FOOTWEAR;
           }
         }
-        console.log("Found a " + foundtype);
+        console.log('Found a ' + foundtype);
         sendSimilarProducts(senderID, matchItem(foundtype, [co[0].w3c.hex, co[1].w3c.hex] ));
         console.log(
           ' ' + co.length + ' ' + ty.length +
@@ -485,7 +485,7 @@ function receivedMessage(event) {
           break;
 
         case 'test':
-          console.log("asdasdas" + matchItem(1, ['#ffffff']));
+          console.log('asdasdas' + matchItem(1, ['#ffffff']));
           sendSimilarProducts(senderID, matchItem(1, ['#ffffff']));
           break;
 
@@ -507,22 +507,22 @@ function receivedMessage(event) {
  *
  */
 function sendHelpOptionsAsButtonTemplates(recipientId) {
-  console.log("[sendHelpOptionsAsButtonTemplates] Sending the help options menu"); 
+  console.log('[sendHelpOptionsAsButtonTemplates] Sending the help options menu'); 
   var messageData = {
     recipient: {
       id: recipientId
     },
     message:{
       attachment:{
-        type:"template",
+        type:'template',
         payload:{
-          template_type:"button",
-          text:"Click the button before to get a list of 3 of our products.",
+          template_type:'button',
+          text:'Click the button before to get a list of 3 of our products.',
           buttons:[
             {
-              "type":"postback",
-              "title":"Get 3 products",
-              "payload":JSON.stringify({action: 'QR_GET_PRODUCT_LIST', limit: 3})
+              'type':'postback',
+              'title':'Get 3 products',
+              'payload':JSON.stringify({action: 'QR_GET_PRODUCT_LIST', limit: 3})
             }
             // limit of three buttons 
           ]
@@ -545,7 +545,7 @@ function handleQuickReplyResponse(event) {
   var message = event.message;
   var quickReplyPayload = message.quick_reply.payload;
   
-  console.log("[handleQuickReplyResponse] Handling quick reply response (%s) from sender (%d) to page (%d) with message (%s)", 
+  console.log('[handleQuickReplyResponse] Handling quick reply response (%s) from sender (%d) to page (%d) with message (%s)', 
     quickReplyPayload, senderID, pageID, JSON.stringify(message));
   
   // use branched conversation with one interaction per feature (each of which contains a variable number of content pieces)
@@ -560,7 +560,7 @@ function handleQuickReplyResponse(event) {
  *
  */
 function respondToHelpRequestWithTemplates(recipientId, requestForHelpOnFeature) {
-  console.log("[respondToHelpRequestWithTemplates] handling help request for %s",
+  console.log('[respondToHelpRequestWithTemplates] handling help request for %s',
     requestForHelpOnFeature);
   var templateElements = [];
 
@@ -570,7 +570,7 @@ function respondToHelpRequestWithTemplates(recipientId, requestForHelpOnFeature)
     var payload = options | {};
     payload = Object.assign(options, {action: action});
     return {
-      "content_type":"text",
+      'content_type':'text',
       title: title,
       payload: JSON.stringify(payload)
     };
@@ -581,18 +581,18 @@ function respondToHelpRequestWithTemplates(recipientId, requestForHelpOnFeature)
       var products = shopify.product.list({ limit: requestPayload.limit});
       products.then(function(listOfProducs) {
         listOfProducs.forEach(function(product) {
-          var url = HOST_URL + "/product.html?id="+product.id;
+          var url = HOST_URL + '/product.html?id='+product.id;
           templateElements.push({
             title: product.title,
             subtitle: product.tags,
             image_url: product.image.src,
             buttons:[
               {
-                "type":"web_url",
-                "url": url,
-                "title":"Read description",
-                "webview_height_ratio": "compact",
-                "messenger_extensions": "true"
+                'type':'web_url',
+                'url': url,
+                'title':'Read description',
+                'webview_height_ratio': 'compact',
+                'messenger_extensions': 'true'
               },
               sectionButton('Get options', 'QR_GET_PRODUCT_OPTIONS', {id: product.id})
             ]
@@ -620,7 +620,7 @@ function respondToHelpRequestWithTemplates(recipientId, requestForHelpOnFeature)
       sh_product.then(function(product) {
         var options = '';
         product.options.map(function(option) {
-          options = options + option.name + ': ' + option.values.join(',') + "\n";
+          options = options + option.name + ': ' + option.values.join(',') + '\n';
         });
         var messageData = {
           recipient: {
@@ -654,18 +654,18 @@ function sendSimilarProducts(recipientId, ids){
       prods.forEach(product => {
         //console.log(JSON.stringify(product));
         //console.log(product.id);
-        var url = HOST_URL + "/product.html?id="+product.id;
+        var url = HOST_URL + '/product.html?id='+product.id;
             templateElements.push({
               title: product.title,
               subtitle: product.tags,
               image_url: product.image.src,
               buttons:[
                 {
-                  "type":"web_url",
-                  "url": url,
-                  "title":"Read description",
-                  "webview_height_ratio": "compact",
-                  "messenger_extensions": "true"
+                  'type':'web_url',
+                  'url': url,
+                  'title':'Read description',
+                  'webview_height_ratio': 'compact',
+                  'messenger_extensions': 'true'
                 },
                 sectionButton('Get options', 'QR_GET_PRODUCT_OPTIONS', {id: product.id})
               ]
@@ -679,9 +679,9 @@ function sendSimilarProducts(recipientId, ids){
           },
           message: {
             attachment: {
-              type: "template",
+              type: 'template',
               payload: {
-                template_type: "generic",
+                template_type: 'generic',
                 elements: templateElements.reverse()
               }
             }
@@ -713,12 +713,12 @@ function receivedDeliveryConfirmation(event) {
 
   if (messageIDs) {
     messageIDs.forEach(function(messageID) {
-      console.log("[receivedDeliveryConfirmation] Message with ID %s was delivered", 
+      console.log('[receivedDeliveryConfirmation] Message with ID %s was delivered', 
         messageID);
     });
   }
 
-  console.log("[receivedDeliveryConfirmation] All messages before timestamp %d were delivered.", watermark);
+  console.log('[receivedDeliveryConfirmation] All messages before timestamp %d were delivered.', watermark);
 }
 
 /*
@@ -737,8 +737,8 @@ function receivedPostback(event) {
   // button for Structured Messages. 
   var payload = event.postback.payload;
 
-  console.log("[receivedPostback] from user (%d) on page (%d) with payload ('%s') " + 
-    "at (%d)", senderID, recipientID, payload, timeOfPostback);
+  console.log('[receivedPostback] from user (%d) on page (%d) with payload (\'%s\') ' + 
+    'at (%d)', senderID, recipientID, payload, timeOfPostback);
 
   respondToHelpRequestWithTemplates(senderID, payload);
 }
@@ -754,7 +754,7 @@ function sendTextMessage(recipientId, messageText) {
     },
     message: {
       text: messageText, // utf-8, 640-character max
-      metadata: "DEVELOPER_DEFINED_METADATA"
+      metadata: 'DEVELOPER_DEFINED_METADATA'
     }
   };
 
@@ -779,16 +779,16 @@ function callSendAPI(messageData) {
       var messageId = body.message_id;
 
       if (messageId) {
-        console.log("[callSendAPI] Successfully sent message with id %s to recipient %s", 
+        console.log('[callSendAPI] Successfully sent message with id %s to recipient %s', 
           messageId, recipientId);
       } else {
-      console.log("[callSendAPI] Successfully called Send API for recipient %s", 
+      console.log('[callSendAPI] Successfully called Send API for recipient %s', 
         recipientId);
       }
     } else {
-      console.error("[callSendAPI] Send API call failed", response.statusCode, response.statusMessage, body.error);
+      console.error('[callSendAPI] Send API call failed', response.statusCode, response.statusMessage, body.error);
     }
-  });  
+  });
 }
 
 /*
@@ -800,41 +800,41 @@ function callSendProfile() {
     qs: { access_token: FB_PAGE_ACCESS_TOKEN },
     method: 'POST',
     json: {
-      "greeting":[
-          {
-          "locale":"default",
-          "text":`Hi there! I'm a bot here to assist you with Candyboxx's Shopify store. To get started, click the "Get Started" button or type "help".`
-          }
-      ] ,
-      "get_started": {
-        "payload": JSON.stringify({action: 'QR_GET_PRODUCT_LIST', limit: 3})
+      greeting: [
+        {
+          locale: 'default',
+          text: 'Hi there! I\'m a bot here to assist you with Candyboxx\'s Shopify store. To get started, click the \'Get Started\' button or type \'help\'.',
+        },
+      ],
+      get_started: {
+        payload: JSON.stringify({ action: 'QR_GET_PRODUCT_LIST', limit: 3 }),
       },
-      "whitelisted_domains":[
-        HOST_URL
-      ]
-    }
+      whitelisted_domains: [
+        HOST_URL,
+      ],
+    },
 
-  }, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log("[callSendProfile]: ", body);
-      var result = body.result;
+  }, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+      console.log('[callSendProfile]: ', body);
+      const { result } = body;
       if (result === 'success') {
-        console.log("[callSendProfile] Successfully sent profile.");
+        console.log('[callSendProfile] Successfully sent profile.');
       } else {
-        console.error("[callSendProfile] There was an error sending profile.");
+        console.error('[callSendProfile] There was an error sending profile.');
       }
     } else {
-      console.error("[callSendProfile] Send profile call failed", response.statusCode, response.statusMessage, body.error);
+      console.error('[callSendProfile] Send profile call failed', response.statusCode, response.statusMessage, body.error);
     }
-  });  
+  });
 }
 
 /*
  * Start server
- * Webhooks must be available via SSL with a certificate signed by a valid 
+ * Webhooks must be available via SSL with a certificate signed by a valid
  * certificate authority.
  */
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), () => {
   console.log('[app.listen] Node app is running on port', app.get('port'));
   callSendProfile();
 });
